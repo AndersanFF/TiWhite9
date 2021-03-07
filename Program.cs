c5c2f2e80bf39ecf3721c1516d4b73c4bc38e483
AndersanFF <alesin0711@gmail.com>
2021-03-07 17:07:07 +0300

    first comm

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+
+namespace _123456789
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int a = 7;
+            int b = 1000;
+            Console.WriteLine("* хруст пальцев");
+            Console.WriteLine("я гуль");
+            for (int c = 0; b > -1;c--)
+            {
+                b = b - a;
+                Console.WriteLine(b);
+                Thread.Sleep(50);
+            }
+            if (b <= 0)
+                Console.WriteLine("Let me die");
+            Console.ReadKey();
+        }
+       
+    }
+   
+}
